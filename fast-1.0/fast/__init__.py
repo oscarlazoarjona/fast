4da40fa018754ee84c0b1235b35055b0066fb3d6
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-01-23 20:16:20 +0000

    Added version and description.

--- fast-1.0/fast/__init__.py
+++ fast-1.0/fast/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 #Oscar Gerardo Lazo Arjona
+"""
+FAST is an acronym for FAST Atomic Spectroscopy from Theory. It calculates
+the density matrix dynamics and steady states of generic alkali atoms,
+rubidium, and cesium using an arbitrary number of states and radiation fields.
+
+"""
+version="1.0"
