6b9adea9486dac92c36eaecce2f665464f37d2bb
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-02-11 19:48:41 +0000

    Added doctest.

--- examples/tests.py
+++ examples/tests.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 ########################################################################
-#Run all doctests:
+# Run all doctests for the core code:
 from doctest import testmod
 import fast
 
-verbose=True#; verbose=False
+verbose=True; verbose=False
 print testmod(fast                   , verbose=verbose)
 print testmod(fast.atomic_structure  , verbose=verbose)
 print testmod(fast.symbolic          , verbose=verbose)
@@ -17,6 +17,15 @@ print testmod(fast.misc              , verbose=verbose)
 print testmod(fast.rk4               , verbose=verbose)
 print testmod(fast.stationary        , verbose=verbose)
 
+########################################################################
+# Run all codes from notebook-generated examples.
+import doctest_08___Three_level_atom_V_symbolic
+import doctest_09___Thermal_States
+import doctest_10___States_database
+
+print testmod(doctest_08___Three_level_atom_V_symbolic  , verbose=verbose)
+print testmod(doctest_09___Thermal_States  , verbose=verbose)
+print testmod(doctest_10___States_database , verbose=verbose)
 ########################################################################
 #Toy examples.
 from toy.two_levels import suite
