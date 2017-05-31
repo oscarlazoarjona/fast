fd54d2fc6fbd7d69c33506a97600c64027120836
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-05-31 21:23:21 +0100

    Implemented installation through setuptools.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#************************************************************************
+# ***********************************************************************
 #       Copyright (C) 2014 - 2017 Oscar Gerardo Lazo Arjona             *
 #              <oscar.lazo@correo.nucleares.unam.mx>                    *
 #                                                                       *
@@ -19,63 +19,110 @@
 #  You should have received a copy of the GNU General Public License    *
 #  along with FAST.  If not, see <http://www.gnu.org/licenses/>.        *
 #                                                                       *
-#************************************************************************
+# ***********************************************************************
 
-########################################################################
-#We build the config.py file
+"""A setuptools based setup module."""
 
-#We get the current directory (from which python setup.py is running).
-import os
-install_dir=os.path.dirname(os.path.abspath("setup.py"))
-#install_dir=install_dir+"/fast"
-print install_dir
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+from fast import __version__
 
-config_text="""#Whether to use parallelization through OpenMP.
-parallel=True; parallel=False
-#Whether to use NETCDF binary files for data communication.
-use_netcdf=True; use_netcdf=False
+here = path.abspath(path.dirname(__file__))
 
-#An integer between 0 and 2 to control which tests are ran.
-run_long_tests=0
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-#The install directory for FAST:
-"""
-config_text+='fast_path="'+install_dir+"/fast"+'"\n'
+setup(
+    name=__version__,
 
-config=file("fast/config.py","w")
-config.write(config_text)
-config.close()
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.2',
 
+    description='FAST is an acronym for FAST Atomic Spectroscopy from Theory.',
+    long_description=long_description,
 
-########################################################################
-#We add an extra line to the .bashrc file.
+    # The project's main homepage.
+    url='https://github.com/oscarlazoarjona/fast',
 
-extra_line='''export PYTHONPATH="${PYTHONPATH}:'''+install_dir+'"'
+    # Author details
+    author='Oscar Gerardo Lazo Arjona',
+    author_email='oscar.lazo@correo.nucleares.unam.mx',
 
+    # Choose your license
+    license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-bashrc_file=file(os.path.expanduser('~')+'/.bashrc',"r")
-bashrc_text=bashrc_file.read()
-bashrc_file.close()
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-if extra_line not in bashrc_text:
-	bashrc_text+="\n"+extra_line+"\n"
-	
-	bashrc_file=file(os.path.expanduser('~')+'/.bashrc',"w")
-	bashrc_file.write(bashrc_text)
-	bashrc_file.close()
-	
-	print "added line to ~/.bashrc:"
-	print extra_line
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
 
-	print "reloading ~/.bashrc"
-	os.system("bash")
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-try:
-	import fast
-	print "Install finished."
-	print "Try:"
-	print "$ python examples/tests.py"
-	print "to verify everything runs smoothly."
-except:
-	raise RuntimeError,'Fast could not be found, something went wrong with the installation.'
-	
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX :: Linux',
+    ],
+
+    # What does your project relate to?
+    keywords='atomic-physics',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    # install_requires=['peppercorn'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
+)
