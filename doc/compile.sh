d5380e5a71cb73246220b9fae3909c9053805cf0
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-10-31 18:27:22 +0000

    Upgraded the compiling script.

--- doc/compile.sh
+++ doc/compile.sh
@@ -2,5 +2,22 @@
 
 # We re-install fast to reflect any changes in the source.
 pip install .. --upgrade
+# We go to the documentation repository:
+cd ../../website/html
+# We empty it.
+git clean -fdx
+
+# We go back to the fast reposotory
+cd ../../fast/doc
+
 # We compile the documentation.
 make html
+
+# We go to the documentation repository:
+cd ../../website/html
+# We add the changes.
+git add .
+# Commit
+git commit -m "Edit."
+# We can push at this point if the result is good with.
+# git push origin gh-pages
