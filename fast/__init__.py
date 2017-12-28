3f0355aed6e6f5de5e9976734fb4a5ec910ff6c1
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-12-28 15:12:28 -0600

    Changed import syntax.

--- fast/__init__.py
+++ fast/__init__.py
@@ -15,23 +15,23 @@ rubidium, and cesium using an arbitrary number of states and radiation fields.
 
 """
 
-from electric_field import PlaneWave, MotField
-from electric_field import electric_field_amplitude_gaussian
-from electric_field import electric_field_amplitude_top
-from electric_field import electric_field_amplitude_intensity
+from electric_field import (PlaneWave, MotField,
+                            electric_field_amplitude_gaussian,
+                            electric_field_amplitude_top,
+                            electric_field_amplitude_intensity)
 
-from misc import Mu, IJ, find_phase_transformation
-from misc import formatLij, convolve_with_gaussian, read_result, fprint
+from misc import (Mu, IJ, find_phase_transformation,
+                  formatLij, convolve_with_gaussian, read_result, fprint,
+                  compile_code)
 
-from graphic import complex_matrix_plot, plot_Lij
-from graphic import Arrow3D, bar_chart_mf, draw_atom3d, draw_mot_field_3d
-from graphic import draw_plane_wave_3d, draw_lasers_3d
-from graphic import draw_state, excitation, decay, draw_multiplet
-from graphic import fancy_matrix_plot, fancy_r_plot, plot_populations
+from graphic import (complex_matrix_plot, plot_Lij,
+                     Arrow3D, bar_chart_mf, draw_atom3d, draw_mot_field_3d,
+                     draw_plane_wave_3d, draw_lasers_3d,
+                     draw_state, excitation, decay, draw_multiplet,
+                     fancy_matrix_plot, fancy_r_plot, plot_populations)
 
 from evolution import write_evolution, run_evolution
 from stationary import write_stationary, run_stationary
-from misc import compile_code
 
 from atomic_structure import (Atom, State, Transition,
                               split_fine_to_hyperfine, split_fine_to_magnetic,
@@ -48,22 +48,19 @@ from atomic_structure import (Atom, State, Transition,
 
 from sympy.core.numbers import Rational as Integer
 
-from sympy import init_printing, pprint
-from sympy import Symbol, Matrix, symbols
-from sympy import conjugate, re, im
-from sympy import simplify, KroneckerDelta, Function, Derivative, solve
+from sympy import (init_printing, pprint, Symbol, Matrix, symbols,
+                   conjugate, re, im, simplify, KroneckerDelta,
+                   Function, Derivative, solve)
 
-from symbolic import define_density_matrix, define_laser_variables
-from symbolic import polarization_vector
-from symbolic import cartesian_to_helicity, helicity_to_cartesian
-from symbolic import helicity_dot_product, cartesian_dot_product
-from symbolic import define_r_components, define_frequencies
-from symbolic import delta_greater, delta_lesser
-from symbolic import ket, bra, ketbra, lindblad_operator, lindblad_terms
-from symbolic import define_psi_coefficients
-from symbolic import define_rho_vector, calculate_A_b
-from symbolic import vector_element, phase_transformation
-from symbolic import wigner_d_small, wigner_d, density_matrix_rotation
+from symbolic import (define_density_matrix, define_laser_variables,
+                      polarization_vector, cartesian_to_helicity,
+                      helicity_to_cartesian, helicity_dot_product,
+                      cartesian_dot_product, define_r_components,
+                      define_frequencies, delta_greater, delta_lesser,
+                      ket, bra, ketbra, lindblad_operator, lindblad_terms,
+                      define_psi_coefficients, define_rho_vector,
+                      calculate_A_b, vector_element, phase_transformation,
+                      wigner_d_small, wigner_d, density_matrix_rotation)
 
 from error_propagation import Measurement
 
