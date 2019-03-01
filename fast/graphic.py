cfe6bed4fb264910760ba846a7fbd9517bb7785d
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2019-03-01 23:01:49 +0000

    Improved complex matrix plot

--- fast/graphic.py
+++ fast/graphic.py
@@ -151,14 +151,18 @@ def fancy_matrix_plot(ax, mat, states=None, path='', name='default.png',
                       take_log=False, colorbar=False, hyperfine_labels=False,
                       center_on_zero=False, **kwds):
     r"""A function to plot matrices labeling axes with atomic states."""
-    Ne = len(mat[0])
     # We transform whatever input we are given into a list of the form
     # [[...]]
     # made of basic float or complex types.
     if complex_matrix:
-        mat = [[complex(mat[i][j]) for j in range(Ne)] for i in range(Ne)]
+        typ = complex
     else:
-        mat = [[float(mat[i][j]) for j in range(Ne)] for i in range(Ne)]
+        typ = float
+    if isinstance(mat[0], list):
+        Ne = len(mat[0][0])
+    else:
+        Ne = mat.shape[0]
+        mat = [[typ(mat[i, j]) for j in range(Ne)] for i in range(Ne)]
 
     rmat = [mat[i] for i in reversed(range(len(mat[0])))]
 
@@ -288,12 +292,15 @@ def fancy_r_plot(r, states=None, path='', name='default.png', y_labels=True,
     # We transform whatever input we are given into a list of the form
     # [[[...]], [[...]], [[...]]]
     # made of basic float or complex types.
-    Ne = len(r[0][0])
     if complex_matrix:
-        r = [[[complex(r[p][i][j]) for j in range(Ne)]
-             for i in range(Ne)] for p in range(3)]
+        typ = complex
     else:
-        r = [[[float(r[p][i][j]) for j in range(Ne)]
+        typ = float
+    if isinstance(r[0], list):
+        Ne = len(r[0][0])
+    else:
+        Ne = r[0].shape[0]
+        r = [[[typ(r[p][i, j]) for j in range(Ne)]
              for i in range(Ne)] for p in range(3)]
 
     nn = 3
