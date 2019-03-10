77eca3f2b1a6130c7c6fd6816d5e465c6bc5d9a7
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2019-03-10 19:19:25 +0000

    Improved PEP8 compliance.

--- fast/stationary.py
+++ fast/stationary.py
@@ -24,11 +24,12 @@
 r"""This module contains the routines or stationary states using Fortran."""
 sage_included = 'sage' in globals().keys()
 if not sage_included:
-    from misc import write_equations_code
+    from misc import write_equations_code, IJ
     from time import time
     import os
 else:
     from time import time
+
 
 def analyze_zeros(row_check, col_check, rhs_check, Ne, N_excluded_mu, states):
     r"""Find zero-valued rows or columns."""
@@ -91,6 +92,7 @@ def analyze_zeros(row_check, col_check, rhs_check, Ne, N_excluded_mu, states):
 
 def write_stationary(path, name, laser, omega, gamma, r, Lij,
                      states=None, excluded_mu=[], verbose=1):
+    r"""Write Fortran code to find the stationary state."""
     t0 = time()
     Ne = len(omega[0])
     Nl = len(laser)
@@ -167,16 +169,16 @@ def write_stationary(path, name, laser, omega, gamma, r, Lij,
     !We write the result to a file.
     """
     if use_netcdf:
-        code0+="""call save_matrix_and_vector('"""+path+name+""".nc',ndelta,"""+str(Ne**2-1)+""",rho,delta)
+        code0 += """call save_matrix_and_vector('"""+path+name+""".nc',ndelta,"""+str(Ne**2-1)+""",rho,delta)
 """
     else:
-        code0+="""open(unit=1,file='"""+path+name+""".dat',status='unknown')
+        code0 += """open(unit=1,file='"""+path+name+""".dat',status='unknown')
     do i=1,ndelta
         write(1,*) delta(i), rho(i,:)
     end do
     close(1)
     """
-    code0+="""
+    code0 += """
 
     deallocate(rho,stat=info)
 
@@ -184,7 +186,7 @@ end program
 """
 
     if use_netcdf:
-        code0+="""
+        code0 += """
 subroutine save_matrix_and_vector(file_name,m,n,matrix,vector)
     use netcdf
     implicit none
@@ -220,7 +222,7 @@ subroutine check(status)
     end if
 end subroutine check
 """
-    code0+="""
+    code0 += """
 subroutine solve(E0,detuning_knob,B,save_systems)
     implicit none
 
@@ -231,11 +233,12 @@ subroutine solve(E0,detuning_knob,B,save_systems)
 """
     ####################################################################
 
-    dummy=write_equations_code(path,name,laser,omega,gamma,r,Lij,
-                                states=states,excluded_mu=excluded_mu,verbose=verbose)
-    code,Nd,row_check,col_check,rhs_check,Ne,N_excluded_mu,states,omega_min,detuningsij,omega_rescaled=dummy
+    dummy = write_equations_code(path, name, laser, omega, gamma, r, Lij,
+                                 states=states, excluded_mu=excluded_mu,
+                                 verbose=verbose)
+    code, Nd, row_check, col_check, rhs_check, Ne, N_excluded_mu, states, omega_min, detuningsij, omega_rescaled = dummy
     ####################################################################
-    code0+="""
+    code0 += """
 	real*8, dimension("""+str(Nd)+""") :: detuning
 
     integer :: INFO,j
@@ -256,17 +259,17 @@ subroutine solve(E0,detuning_knob,B,save_systems)
 #    	print* \n"""
     ####################################################################
     # We make LAPACK solve the damn thing.
-    code+="	if (save_systems) then\n"
-    code+="		open(file='"+path+name+"_AB.dat',unit=4,status='unknown')\n"
-    code+="		do j=1,"+str(Ne**2-1-N_excluded_mu)+'\n'
-    code+="			write(4,*) A(j,:),B(j,1)\n"
-    code+="		end do\n"
-    code+="		close(4)\n"
-    code+="	end if\n\n"
+    code += "	if (save_systems) then\n"
+    code += "		open(file='"+path+name+"_AB.dat',unit=4,status='unknown')\n"
+    code += "		do j=1,"+str(Ne**2-1-N_excluded_mu)+'\n'
+    code += "			write(4,*) A(j,:),B(j,1)\n"
+    code += "		end do\n"
+    code += "		close(4)\n"
+    code += "	end if\n\n"
 
-    code+='	call dgesv('+str(Ne**2-1-N_excluded_mu)+', 1, A, '+str(Ne**2-1-N_excluded_mu)+', IPIV, B, '+str(Ne**2-1-N_excluded_mu)+', INFO)\n'
+    code += '	call dgesv('+str(Ne**2-1-N_excluded_mu)+', 1, A, '+str(Ne**2-1-N_excluded_mu)+', IPIV, B, '+str(Ne**2-1-N_excluded_mu)+', INFO)\n'
     # code+="	print*,'INFO',INFO\n"
-    code+="""	if (INFO>0) B=-11\n"""
+    code += """	if (INFO>0) B=-11\n"""
     # code+="""	if (INFO>0) print*, 'For frequencies',detuning_knob,'The system could not be solved, exit code:',INFO\n"""
     # code+="""	if (INFO>0) then\n"""
     # code+="""		do j="""+str(Ne**2-1)+"""\n"""
@@ -276,78 +279,82 @@ subroutine solve(E0,detuning_knob,B,save_systems)
     ####################################################################
     # We check the rows and columns searching for rows of zeros and
     # columns of zeros.
-    excluded_mu=analyze_zeros(row_check,col_check,rhs_check,Ne,N_excluded_mu,states)
+    excluded_mu = analyze_zeros(row_check, col_check, rhs_check, Ne, N_excluded_mu, states)
 
-    if excluded_mu!=[]:
-        t0=time()-t0
-        t_extra=write_stationary(path,name,laser,omega,gamma,r,Lij,
-                                use_symbolic_phase_transformation=True,states=states,excluded_mu=excluded_mu)
+    if excluded_mu != []:
+        t0 = time()-t0
+        t_extra = write_stationary(path, name, laser, omega, gamma, r, Lij,
+                                   use_symbolic_phase_transformation=True,
+                                   states=states, excluded_mu=excluded_mu)
 
         return t_extra+t0
     else:
-        f=file(path+name+'.f90','w')
-        code=code0+code+'end subroutine\n'
+        f = file(path+name+'.f90', 'w')
+        code = code0+code+'end subroutine\n'
         f.write(code)
         f.close()
 
         return time()-t0
 
 
-def run_stationary(path, name, E0, laser_frequencies, spectrum_of_laser, N_delta,
-                   frequency_step=None, frequency_end=None, print_steps=False,
-                   specific_deltas=None, save_systems=False,clone=None,use_netcdf=True):
-    t0=time()
+def run_stationary(path, name, E0, laser_frequencies, spectrum_of_laser,
+                   N_delta, frequency_step=None, frequency_end=None,
+                   print_steps=False, specific_deltas=None,
+                   save_systems=False, clone=None, use_netcdf=True):
+    r"""Run an executable for the stationary state."""
+    t0 = time()
 
-    params=''.join([str(i)+' ' for i in E0])+'\n'
-    params+=''.join([str(i)+' ' for i in laser_frequencies])+'\n'
-    params+=str(spectrum_of_laser)+'\n'
-    params+=str(N_delta)+'\n'
+    params = ''.join([str(i)+' ' for i in E0])+'\n'
+    params += ''.join([str(i)+' ' for i in laser_frequencies])+'\n'
+    params += str(spectrum_of_laser)+'\n'
+    params += str(N_delta)+'\n'
 
-    if frequency_end !=None:
-        if frequency_step !=None:
-            raise ValueError,'both frequency_end and frequency_step were specified.'
-        if N_delta==1:
-            frequency_step=0.0
+    if frequency_end is not None:
+        if frequency_step is not None:
+            aux = 'both frequency_end and frequency_step were specified.'
+            raise ValueError(aux)
+        if N_delta == 1:
+            frequency_step = 0.0
         else:
-            frequency_step=(frequency_end-laser_frequencies[spectrum_of_laser-1])/(N_delta-1)
+            frequency_step = (frequency_end-laser_frequencies[spectrum_of_laser-1])/(N_delta-1)
 
-    params+=str(frequency_step)+'\n'
+    params += str(frequency_step)+'\n'
     if print_steps:
-        params+='.true.\n'
+        params += '.true.\n'
     else:
-        params+='.false.\n'
+        params += '.false.\n'
 
     if save_systems:
-        params+='.true.\n'
+        params += '.true.\n'
     else:
-        params+='.false.\n'
+        params += '.false.\n'
 
     if use_netcdf:
-        params+='.true.\n'
+        params += '.true.\n'
     else:
-        params+='.false.\n'
+        params += '.false.\n'
 
-    if specific_deltas==None:
-        params+='.false.\n'
+    if specific_deltas is None:
+        params += '.false.\n'
     else:
-        params+='.true.\n'
-        params+=''.join([str(delta)+' ' for delta in specific_deltas])
+        params += '.true.\n'
+        params += ''.join([str(delta)+' ' for delta in specific_deltas])
 
-    if clone!=None:
-        clone='_'+str(clone)
+    if clone is not None:
+        clone = '_'+str(clone)
     else:
-        clone=''
+        clone = ''
 
-    f=file(path+name+'_params'+clone+'.dat','w')
+    f = file(path+name+'_params'+clone+'.dat', 'w')
     f.write(params)
     f.close()
 
-    com=path+name+clone
-    #print 'running',com
-    exit_code=os.system(com)
+    com = path+name+clone
+    # print 'running',com
+    exit_code = os.system(com)
     if exit_code != 0:
-        s='command '+com+' returned exit code '+str(exit_code)
-        raise RuntimeError,s
+        s = 'command '+com+' returned exit code '+str(exit_code)
+        raise RuntimeError(s)
     return time()-t0
 
 
